efe39e04f621d49ce77d6fff2b18e1a46598f9e8
Florencia Alcoba <134442653+falcoba98@users.noreply.github.com>
2024-11-14 21:34:45 -0300

    Revert "añado campos tarjeta y cuenta banc"

--- js/cart.js
+++ js/cart.js
@@ -15,37 +15,38 @@ document.addEventListener("DOMContentLoaded", function() {
           <div class="col-3 header-etiquetas d-none d-sm-none d-md-block">Subtotal</div>
         </div>`;
 
-      cartItems.forEach((item, index) => {
-        cartContainer.innerHTML += `
-          <div class="card ms-1 me-1 ms-md-5 me-md-5 me-1 cart-item shadow-sm" id="cart-item-${index}">
-            <div class="row g-0">
-              <div class="col-lg-2 col-md-12 col-sm-12 d-flex align-items-center justify-content-start">
-                <img src="${item.images[0]}" class="cart-img" alt="${item.name}">
-              </div>
-              <div class="col-lg-10 col-md-12 col-sm-12">
-                <div class="row height-100 d-flex align-items-center w-100">
-                   <div class="col-lg-4 col-md-4 text-center text-md-start text-sm-center d-flex align-items-center">
-                     <div class="card-title">${item.name}</div>
-                   </div>
-                   <div class="col-lg-2 col-md-2 text-center text-md-start text-sm-center">
-                     <p class="card-text">${item.currency} ${item.cost}</p>
-                   </div>
-                   <div class="col-lg-2 col-md-2 text-center text-md-start text-sm-center">
-                     <input type="number" class="cart-quantity" min="0" value="${item.quantity || 1}" data-index="${index}">
-                   </div>
-                   <div class="col-lg-2 col-md-3 text-center text-md-start text-sm-center">
-                     <p class="subtotal" id="subtotal-${index}">${item.currency} ${(item.cost * (item.quantity || 1)).toFixed(2)}</p>
-                   </div>
-                   <div class="col-lg-2 col-md-1 text-lg-end text-md-center text-sm-center text-center">
-                     <button class="btn btn-link text-secondary pe-ls-5 pe-md-0 ps-sm-0 pe-sm-0 elimiar-item" onclick="eliminarProducto(${index})">
-                       <img src="img/papelera-de-reciclaje.png" alt="Eliminar" width="30">
-                     </button>
-                   </div>
+        cartItems.forEach((item, index) => {
+          cartContainer.innerHTML += `
+            <div class="card ms-1 me-1 ms-md-5 me-md-5 me-1 cart-item shadow-sm" id="cart-item-${index}">
+              <div class="row g-0">
+                <div class="col-lg-2 col-md-12 col-sm-12 d-flex align-items-center justify-content-start">
+                  <img src="${item.images[0]}" class="cart-img" alt="${item.name}">
+                </div>
+                <div class="col-lg-10 col-md-12 col-sm-12">
+                  <div class="row height-100 d-flex align-items-center w-100">
+                     <div class="col-lg-4 col-md-4 text-center text-md-start text-sm-center d-flex align-items-center">
+                       <div class="card-title">${item.name}</div>
+                     </div>
+                     <div class="col-lg-2 col-md-2 text-center text-md-start text-sm-center">
+                       <p class="card-text">${item.currency} ${item.cost}</p>
+                     </div>
+                     <div class="col-lg-2 col-md-2 text-center text-md-start text-sm-center">
+                       <input type="number" class="cart-quantity" min="0" value="${item.quantity || 1}" data-index="${index}">
+                     </div>
+                     <div class="col-lg-2 col-md-3 text-center text-md-start text-sm-center">
+                       <p class="subtotal" id="subtotal-${index}">${item.currency} ${(item.cost * (item.quantity || 1)).toFixed(2)}</p>
+                     </div>
+                     <div class="col-lg-2 col-md-1 text-lg-end text-md-center text-sm-center text-center">
+                       <button class="btn btn-link text-secondary pe-ls-5 pe-md-0 ps-sm-0 pe-sm-0 elimiar-item" onclick="eliminarProducto(${index})">
+                         <img src="img/papelera-de-reciclaje.png" alt="Eliminar" width="30">
+                       </button>
+                     </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>`;
-      });
+            </div>`;
+        });
+        
 
       // Sección de tipos de envío
       cartContainer.innerHTML += `
@@ -141,100 +142,190 @@ document.addEventListener("DOMContentLoaded", function() {
           </div>
         </div>`;
 
-      // Crear los campos dinámicos para la tarjeta de crédito y transferencia bancaria
-      const camposCredito = document.createElement("div");
-      camposCredito.id = "campos-credito";
-      camposCredito.style.display = "none";
-      camposCredito.innerHTML = `
-        <input type="text" class="form-control campoPago" id="titular" placeholder="Titular" required>
-        <input type="text" class="form-control campoPago" id="numero-tarjeta" placeholder="Número de tarjeta" required>
-        <input type="text" class="form-control campoPago" id="fecha-expiracion" placeholder="Fecha de expiración (MM/AA)" required>
-        <input type="text" class="form-control campoPago" id="codigo-seguridad" placeholder="Código de seguridad (CVV)" required>`;
-      cartContainer.appendChild(camposCredito);
+      // Sección de resumen de costos
+      cartContainer.innerHTML += `
+        <div class="card ms-1 me-1 ms-md-5 me-md-5 mb-3 mt-5 cart-item shadow-sm p-4">
+          <div class="row g-0">
+            <div class="col-md-12">
+              <div class="row height-100 d-flex align-items-center">
+                <div class="col-4 text-center" id="subtotal">
+                  <b>Subtotal: $0.00</b>
+                </div>
+                <div class="col-4 text-center" id="costo-envio">
+                  <b>Costo de envío: $0.00</b>
+                </div>
+                <div class="col-4 text-center" id="total-final">
+                  <b>Total: $0.00</b>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>`;
 
-      const camposTransferencia = document.createElement("div");
-      camposTransferencia.id = "campos-transferencia";
-      camposTransferencia.style.display = "none";
-      camposTransferencia.innerHTML = `
-        <p class="form-control campoPago" id="numero-cuenta" required> 123857281-1 </p>
-        <p class="form-control campoPago" id="banco" required> Itaú </p>`;
-      cartContainer.appendChild(camposTransferencia);
-
-      // Mostrar u ocultar los campos de pago según la selección
-      const tarjetaCreditoRadio = document.getElementById("credito");
-      const transferenciaRadio = document.getElementById("expira");
-
-      function mostrarCamposPago() {
-        camposCredito.style.display = tarjetaCreditoRadio.checked ? "block" : "none";
-        camposTransferencia.style.display = transferenciaRadio.checked ? "block" : "none";
-      }
-
-      tarjetaCreditoRadio.addEventListener("change", mostrarCamposPago);
-      transferenciaRadio.addEventListener("change", mostrarCamposPago);
-
-      // Llamar a la función de mostrar campos al cargar la página (si ya hay una opción seleccionada)
-      mostrarCamposPago();
-      
-      // Añadir el botón de "Finalizar compra"
+      // Botón de finalizar compra
       const finalizarCompraBtn = document.createElement("button");
       finalizarCompraBtn.innerText = "Finalizar compra";
-      finalizarCompraBtn.className = "btn btn-warning mt-3 finalizar-compra";
+      finalizarCompraBtn.className = "btn btn-warning mt-3 finalizar-compra"; 
+      //finalizarCompraBtn.style.width = "20%";
+      //finalizarCompraBtn.style.marginLeft = "39%";
+
       finalizarCompraBtn.onclick = function() {
-        // Validación
+        // Validación de los campos de dirección
         const departamento = document.getElementById("departamento").value.trim();
         const calle = document.getElementById("calle").value.trim();
         const numero = document.getElementById("numero").value.trim();
         const esquina = document.getElementById("esquina").value.trim();
-        
+
         if (!departamento || !calle || !numero || !esquina) {
           alert("Por favor, completa todos los campos de la dirección de envío.");
           return;
         }
 
+        // Validación de la forma de envío
         const envioSeleccionado = document.querySelector("input[name='tipo']:checked");
         if (!envioSeleccionado) {
           alert("Por favor, selecciona un tipo de envío.");
           return;
         }
 
+        // Validación de cantidad de productos
+        const cantidades = document.querySelectorAll(".cart-quantity");
+        for (let input of cantidades) {
+          if (parseInt(input.value) <= 0) {
+            alert("La cantidad de cada producto debe ser mayor a 0.");
+            return;
+          }
+        }
+
+        // Validación de la forma de pago
         const pagoSeleccionado = document.querySelector("input[name='pago']:checked");
         if (!pagoSeleccionado) {
           alert("Por favor, selecciona una forma de pago.");
           return;
         }
 
-        if (pagoSeleccionado.id === "credito") {
-          const titular = document.getElementById("titular").value.trim();
-          const numeroTarjeta = document.getElementById("numero-tarjeta").value.trim();
-          const fechaExpiracion = document.getElementById("fecha-expiracion").value.trim();
-          const codigoSeguridad = document.getElementById("codigo-seguridad").value.trim();
-          if (!numeroTarjeta || !fechaExpiracion || !codigoSeguridad) {
-            alert("Por favor, completa todos los campos de la tarjeta de crédito.");
-            return;
-          }
-        } else if (pagoSeleccionado.id === "expira") {
-          const numeroCuenta = document.getElementById("numero-cuenta").value.trim();
-          const banco = document.getElementById("banco").value.trim();
-          if (!numeroCuenta || !banco) {
-            alert("Por favor, completa todos los campos de la transferencia bancaria.");
+        // Validación de los campos específicos de la forma de pago
+        const camposPago = document.querySelectorAll(".campoPago");
+        for (let campo of camposPago) {
+          if (campo.value.trim() === "") {
+            alert("Por favor, completa todos los campos de la forma de pago seleccionada.");
             return;
           }
         }
 
-        // Si todo está correcto
+        // Si todas las validaciones son correctas, muestra mensaje de éxito
         alert("Compra realizada exitosamente. ¡Gracias por tu compra!");
       };
-
       cartContainer.appendChild(finalizarCompraBtn);
 
-      // Actualización del badge del carrito
-      actualizarBadgeCarrito();
+      // Evento para actualizar subtotal y total cuando cambia la cantidad
+      document.querySelectorAll(".cart-quantity").forEach(input => {
+        input.addEventListener("input", function() {
+          const index = input.getAttribute("data-index");
+          const newQuantity = parseInt(input.value);
+          cartItems[index].quantity = newQuantity;
+          localStorage.setItem("cartItems", JSON.stringify(cartItems));
+          actualizarSubtotal(index, newQuantity, cartItems[index].cost, cartItems[index].currency);
+          actualizarTotal();
+        });
+      });
+
+      // Evento para recalcular total cuando cambia el tipo de envío
+      document.querySelectorAll("input[name='tipo']").forEach(radio => {
+        radio.addEventListener("change", actualizarTotal);
+      });
+
+      actualizarTotal();
+    } else {
+      mostrarCarritoVacio(cartContainer);
     }
   }
 });
 
-function actualizarBadgeCarrito() {
+function mostrarCarritoVacio(cartContainer) {
+  cartContainer.innerHTML = `
+    <div class="text-center" role="alert">
+      <h4 class="alert-heading">Tu carrito está vacío</h4>
+      <p>Agrega productos para verlos aquí.</p>
+    </div>`;
+}
+
+
+// Definir la tasa de cambio (por ejemplo, 1 moneda local = 41 USD)
+const tasaDeCambio = 41; // Suponiendo que estás trabajando con una moneda local
+
+function convertirAPrecioEnDolares(costo, moneda) {
+  // Convertir el precio a dólares si la moneda es diferente a USD
+  if (moneda !== 'USD') {
+    return costo / tasaDeCambio;
+  }
+  return costo; // Si ya está en USD, no hacer ninguna conversión
+}
+
+function actualizarSubtotal(index, quantity, cost, currency) {
+  const subtotalElement = document.getElementById(`subtotal-${index}`);
+  
+  // Convertir el costo a dólares
+  const costoEnDolares = convertirAPrecioEnDolares(cost, currency);
+  
+  // Calcular el subtotal en dólares
+  const subtotal = quantity * costoEnDolares;
+  
+  // Mostrar el subtotal convertido
+  subtotalElement.textContent = `USD ${subtotal.toFixed(2)}`;
+}
+
+function actualizarTotal() {
   const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+  let subtotal = 0;
+
+  cartItems.forEach(item => {
+    // Convertir cada precio a dólares antes de sumarlo al subtotal
+    const costoEnDolares = convertirAPrecioEnDolares(item.cost, item.currency);
+    subtotal += costoEnDolares * (item.quantity || 1);
+  });
+
+  const tipoEnvio = document.querySelector("input[name='tipo']:checked");
+  let costoEnvio = 0;
+
+  if (tipoEnvio) {
+    switch (tipoEnvio.id) {
+      case "premium":
+        costoEnvio = subtotal * 0.15;
+        break;
+      case "express":
+        costoEnvio = subtotal * 0.07;
+        break;
+      case "Standard":
+        costoEnvio = subtotal * 0.05;
+        break;
+    }
+  }
+
+  const total = subtotal + costoEnvio;
+  document.getElementById("subtotal").textContent = `Subtotal: USD ${subtotal.toFixed(2)}`;
+  document.getElementById("costo-envio").textContent = `Costo de envío: USD ${costoEnvio.toFixed(2)}`;
+  document.getElementById("total-final").textContent = `Total: USD ${total.toFixed(2)}`;
+
+  // Actualizar el badge del carrito
+  actualizarBadgeCarrito();
+}
+
+function eliminarProducto(index) {
+  let cart = JSON.parse(localStorage.getItem("cartItems")) || [];
+  cart.splice(index, 1);
+  localStorage.setItem("cartItems", JSON.stringify(cart));
+  document.getElementById(`cart-item-${index}`).remove();
+  actualizarTotal();
+}
+
+// Función para actualizar el badge del carrito
+function actualizarBadgeCarrito() {
+  let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
   const totalQuantity = cartItems.reduce((sum, item) => sum + (item.quantity || 1), 0);
   document.getElementById("cartCountBadge").innerText = totalQuantity;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  actualizarBadgeCarrito();
+});
